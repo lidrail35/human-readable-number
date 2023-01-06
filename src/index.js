08408428ec5d5a87c603111e75e4416c7d092205
lidrail35 <lidrail35@mail.ru>
2023-01-06 14:40:00 +0300

    feat: add solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
+  const numberFirst = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const numberSecond = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+  if ((number >= 100) & (number < 1000)) { 
+    return (numberFirst[Math.floor(number/100)] + ' hundred' + (number%100 ? ' ' + toReadable(number%100):'')); 
+  }
+   
+  if ((number >= 20) & (number < 100)) { 
+    return (numberSecond[Math.floor(number/10)-2] + (number%10 ? ' ' + toReadable(number%10):''));
+  }
   
-}
+  if ((number >= 0) & (number < 20)) { return numberFirst[number]};
+  
+  }
